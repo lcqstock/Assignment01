49fc3a87877d713e15d15751a159bf5f61b04cc0
lcqstock <liucq2010@hotmail.com>
2025-09-24 08:07:37 -0700

    Step 1: Convert the following highlighted identifiers to Camel Case notation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Step 1: Convert the following highlighted identifiers to Camel Case notation
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let MyLibraryFunction;
+*/
+
