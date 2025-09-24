a90582e437e58eef41280478113e000e7666804d
lcqstock <liucq2010@hotmail.com>
2025-09-24 08:06:21 -0700

    Initial Commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// STEP 1: Convert the following highlighted identifiers to Camel Case notation
-/*
-let someMonth;
-function theMonth();
-let currentMonth;
-let summerMonth;
-let MyLibraryFunction;
-*/
-
